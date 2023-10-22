55d56f575eaf8ba7a8c76a8e72be790d188825ab
nikoletaxvs <nikoletaxvs@gmail.com>
2023-10-22 12:32:43 +0300

    changed menuformview

--- Controllers/CookController.cs
+++ Controllers/CookController.cs
@@ -1,32 +1,50 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ThesisOct2023.Models;
+using ThesisOct2023.Models.ViewModels;
 using ThesisOct2023.Repositories;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace ThesisOct2023.Controllers
 {
-    [Authorize(Roles ="Cook")]
+    [Authorize(Roles = "Cook")]
     public class CookController : Controller
     {
         private readonly IFoodRepository _foodRepository;
         public CookController(IFoodRepository foodRepository)
         {
-            _foodRepository= foodRepository;
+            _foodRepository = foodRepository;
         }
         public IActionResult Index()
         {
             return View();
         }
-        public  IActionResult Food()
+        public IActionResult Food()
         {
-            IEnumerable<Food> food= _foodRepository.GetAllFood();
+            IEnumerable<Food> food = _foodRepository.GetAllFood();
             return View(food);
         }
-        public IActionResult WeeksMenu() {
+        public IActionResult WeeksMenu()
+        {
             return View();
         }
-        public IActionResult CreateMenu() {
-            return View();
+        public IActionResult CreateMenu()
+        {
+            var menuItemsData = _foodRepository.GetAllFood();
+            var model = new MenuFormView();
+            model.ItemsSelectList = new List<SelectListItem>();
+
+            foreach (var item in menuItemsData)
+            {
+                model.ItemsSelectList.Add(new SelectListItem { Text = item.Title, Value = item.Id.ToString() });
+            }
+
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult PostMenu(MenuFormView model)
+        {
+
+            return RedirectToAction("Index");
         }
     }
 }
