d7036a582adfb46b9c39e0568f274a3c6e884aa4
nikoletaxvs <nikoletaxvs@gmail.com>
2023-11-30 12:18:11 +0200

    Roles controller has been automated, roles get seeded in app initialization

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -5,32 +5,32 @@ namespace ThesisOct2023.Controllers
 {
     public class RolesController : Controller
     {
-        private readonly RoleManager<IdentityRole> _manager;
-        public RolesController(RoleManager<IdentityRole> roleManager)
-        {
-            _manager= roleManager;  
-        }
-        public IActionResult Index()
-        {
-            //List of roles
-            var roles = _manager.Roles; 
-            return View(roles);
-        }
-        [HttpGet]
-        public IActionResult Create()
-        {
+        //private readonly RoleManager<IdentityRole> _manager;
+        //public RolesController(RoleManager<IdentityRole> roleManager)
+        //{
+        //    _manager= roleManager;  
+        //}
+        //public IActionResult Index()
+        //{
+        //    //List of roles
+        //    var roles = _manager.Roles; 
+        //    return View(roles);
+        //}
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
             
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(IdentityRole role)
-        {
-            //Check if the role exists
-            if (!_manager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
-            {
-                _manager.CreateAsync(new IdentityRole(role.Name)).GetAwaiter().GetResult();
-            }
-            return RedirectToAction("Index");
-        }
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult Create(IdentityRole role)
+        //{
+        //    //Check if the role exists
+        //    if (!_manager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
+        //    {
+        //        _manager.CreateAsync(new IdentityRole(role.Name)).GetAwaiter().GetResult();
+        //    }
+        //    return RedirectToAction("Index");
+        //}
     }
 }
