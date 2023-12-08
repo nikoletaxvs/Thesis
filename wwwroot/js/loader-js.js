97b9faeae443e880e42a121a5319a36436f6f7e2
nikoletaxvs <nikoletaxvs@gmail.com>
2023-12-08 15:48:12 +0200

    added waiting animation for page load

---
+++ wwwroot/js/loader-js.js
@@ -0,0 +1,11 @@
+﻿var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    setTimeout(() => {
+        loader.style.height = "100%";
+        loader.style.width = "100%";
+        loader.style.borderRadius = "50%";
+        loader.style.visibility = "hidden";
+    }, 250);
+   
+   
+});
